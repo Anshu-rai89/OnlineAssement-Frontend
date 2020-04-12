ff154a9d4adc09e5e5a3c41d8a8dbb99f91f0a76
Anshu <raianshu1998@gmail.com>
2020-04-12 05:46:10 -0400

    design componennt structure and added react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter} from 'react-router-dom';
 
 function App() {
+
+  
   return (
+    <BrowserRouter>
     <div className="App">
       <h1>setup</h1>
     </div>
+    </BrowserRouter>
   );
 }
 
