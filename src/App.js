806e01511b9c4d59710807e835e3cc3a30a470fc
Anshu <raianshu1998@gmail.com>
2020-04-12 17:06:01 -0400

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
 
+import Navbar from './components/layout/navbar';
+
 function App() {
 
   
   return (
     <BrowserRouter>
     <div className="App">
+      <Navbar />
       <h1>setup</h1>
     </div>
     </BrowserRouter>
